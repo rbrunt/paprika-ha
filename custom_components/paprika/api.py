6f13f7fd65b591d286dd77e53b4b220c490d6b1c
Richard Brunt <1533730+rbrunt@users.noreply.github.com>
2025-03-23 22:13:29 +0000

    Finally Working

--- custom_components/paprika/api.py
+++ custom_components/paprika/api.py
@@ -8,16 +8,15 @@ import aiohttp
 _LOGGER = logging.getLogger(__name__)
 
 
-
 MealId = NewType("MealId", str)
 RecipeID = NewType("RecipeID", str)
+
 
 class MealType(Enum):
     Breakfast = 0
     Lunch = 1
     Dinner = 2
     Snack = 3
-    
 
 
 class PlannedMeal(TypedDict):
@@ -27,13 +26,12 @@ class PlannedMeal(TypedDict):
     type: MealType
     name: str
     order_flag: int
-    type_uid: str # TODO
+    type_uid: str  # TODO
     scale: Optional[int]
     is_ingredient: bool
 
 
 class PaprikaApi:
-
     def __del__(self):
         # TODO: verify this works as expected when erroring during setup.
         self.session.close()
@@ -43,25 +41,26 @@ class PaprikaApi:
         self.access_token = token
         self.session = aiohttp.ClientSession("https://www.paprikaapp.com/api/v2/")
         self.session.headers["authorization"] = f"Bearer {self.access_token}"
-    
+
     @classmethod
     async def login(cls, email: str, password: str):
         """Use a username and password to get a token that can be used to initialise the client for other calls."""
-        async with aiohttp.ClientSession() as session: 
-            response = await session.post("https://paprikaapp.com/api/v1/account/login", data={"email": email, "password": password})
-            json_response =  await response.json()
+        async with aiohttp.ClientSession() as session:
+            response = await session.post(
+                "https://paprikaapp.com/api/v1/account/login",
+                data={"email": email, "password": password},
+            )
+            json_response = await response.json()
             # TODO: remove this once confirmed working:
             _LOGGER.warning(json.dumps(json_response))
             return json_response["result"]["token"]
-
 
     async def get_meals(self):
         response = await self.session.get("sync/meals")
         response.raise_for_status()
         response_json = await response.json()
         meals: list[PlannedMeal] = []
-        for meal in response_json:
-            meal["dat"]
+        for meal in response_json["result"]:
             meal["date"] = datetime.strptime(meal["date"][:10], "%Y-%m-%d").date()
             meal["type"] = MealType(meal["type"])
             meals.append(cast(PlannedMeal, meal))
--- custom_components/paprika/calendar.py
+++ custom_components/paprika/calendar.py
@@ -1,36 +1,71 @@
+import logging
 from typing import TYPE_CHECKING
 from homeassistant.components.calendar import CalendarEntity, CalendarEvent
 
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+import homeassistant.util.dt
 from datetime import datetime, timedelta
 
+
+from .api import MealType, PlannedMeal
 
 if TYPE_CHECKING:
     from .coordinator import PaprikaCoordinator
     from .data import PaprikaConfigEntry
-    from .api import MealType, PlannedMeal
     from homeassistant.core import HomeAssistant
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 
-def filter_by_type(m: PlannedMeal, meal_type: MealType) -> bool:
-    return m["type"] == meal_type
+LOGGER = logging.getLogger(__name__)
 
 
-def filter_between_dates(
-    m: PlannedMeal, start_date: datetime, end_date: datetime
-) -> bool:
-    return start_date <= m["date"] < end_date
-
-
-class PaprikaMealCalendar(CalendarEntity, CoordinatorEntity[PaprikaCoordinator]):
-
-    def __init__(self, coordinator: PaprikaCoordinator, meal_type: MealType):
+class PaprikaMealCalendar(CalendarEntity, CoordinatorEntity["PaprikaCoordinator"]):
+    def __init__(self, coordinator: "PaprikaCoordinator", meal_type: MealType):
         super().__init__(coordinator)
         self.meal_type = meal_type
-        self.entity_description = f"Calendar for {meal_type.name}"
+        # self.entity_description = f"Calendar for {meal_type.name}"
+        self._attr_has_entity_name = False
 
-    def async_get_events(self, hass, start_date, end_date):
+    @property
+    def name(self):
+        """Name of the entity."""
+        return f"{self.meal_type.name}"
+
+    @property
+    def event(self):
+        LOGGER.info("@@@ Getting event")
+
+        if not self.coordinator.meals and len(self.coordinator.meals) > 0:
+            return None
+
+        def filter_by_type(m: PlannedMeal) -> bool:
+            return m["type"] == self.meal_type
+
+        meals = filter(filter_by_type, self.coordinator.meals)
+        today = homeassistant.util.dt.now().date()
+        meal = list(filter(lambda x: x["date"] == today, meals))
+        LOGGER.info(meals)
+        LOGGER.info(meal)
+        if len(meal) >= 1:
+            return CalendarEvent(
+                start=meal[0]["date"],
+                end=meal[0]["date"] + timedelta(days=1),
+                summary=meal[0]["name"],
+                description=meal[0]["name"],
+            )
+        else:
+            return None
+
+    async def async_get_events(self, hass, start_date, end_date):
+        LOGGER.info("@@@ Getting events")
+        LOGGER.info(start_date)
+        LOGGER.info(end_date)
+
+        def filter_by_type(m: PlannedMeal) -> bool:
+            return m["type"] == self.meal_type
+
+        def filter_between_dates(m: PlannedMeal) -> bool:
+            return start_date.date() <= m["date"] < end_date.date()
 
         meals = filter(filter_by_type, self.coordinator.meals)
         meals_in_range = filter(filter_between_dates, meals)
@@ -47,9 +82,9 @@ class PaprikaMealCalendar(CalendarEntity, CoordinatorEntity[PaprikaCoordinator])
 
 
 async def async_setup_entry(
-    hass: HomeAssistant,  # noqa: ARG001 Unused function argument: `hass`
-    entry: PaprikaConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    hass: "HomeAssistant",  # noqa: ARG001 Unused function argument: `hass`
+    entry: "PaprikaConfigEntry",
+    async_add_entities: "AddEntitiesCallback",
 ) -> None:
     """Set up the sensor platform."""
     async_add_entities(
