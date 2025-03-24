fad226abb31f2f2cdb128b364b6381551c9eabc1
Richard Brunt <1533730+rbrunt@users.noreply.github.com>
2025-03-24 21:54:18 +0000

    Fix

--- custom_components/paprika/coordinator.py
+++ custom_components/paprika/coordinator.py
@@ -1,6 +1,3 @@
-
-
-
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
@@ -8,21 +5,19 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 if TYPE_CHECKING:
     from .data import PaprikaConfigEntry, PlannedMeal
 
+
 @dataclass
 class PaprikaData:
-    meals: list['PlannedMeal']
-    recipe_count: int
+    meals: list["PlannedMeal"]
 
 
 class PaprikaCoordinator(DataUpdateCoordinator[PaprikaData]):
     """Class to manage fetching data from the API."""
 
-    config_entry: 'PaprikaConfigEntry'
+    config_entry: "PaprikaConfigEntry"
 
     async def _async_update_data(self) -> Any:
         """Update data via library."""
         meals = await self.config_entry.runtime_data.client.get_meals()
-        recipe_count = await self.config_entry.runtime_data.client.get_recipe_count()
-        data = PaprikaData(meals = meals, recipe_count= recipe_count)
+        data = PaprikaData(meals=meals)
         return data
-        
