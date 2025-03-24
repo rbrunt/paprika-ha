f5193f6b5818725539ca918a69d783322263151e
Richard Brunt <1533730+rbrunt@users.noreply.github.com>
2025-03-24 21:59:36 +0000

    Add unique id to calendar entities

--- custom_components/paprika/calendar.py
+++ custom_components/paprika/calendar.py
@@ -20,10 +20,10 @@ LOGGER = logging.getLogger(__name__)
 
 
 class PaprikaMealCalendar(CalendarEntity, CoordinatorEntity["PaprikaCoordinator"]):
-    def __init__(self, coordinator: "PaprikaCoordinator", meal_type: MealType):
+    def __init__(self, coordinator: "PaprikaCoordinator", entry: "PaprikaConfigEntry", meal_type: MealType):
         super().__init__(coordinator)
         self.meal_type = meal_type
-        # self.entity_description = f"Calendar for {meal_type.name}"
+        self._attr_unique_id = f"{entry.title}_calendar_{self.meal_type.name}"
         self._attr_has_entity_name = False
 
     @property
@@ -90,16 +90,16 @@ async def async_setup_entry(
     async_add_entities(
         [
             PaprikaMealCalendar(
-                coordinator=entry.runtime_data.coordinator, meal_type=MealType.Dinner
+                coordinator=entry.runtime_data.coordinator, entry=entry, meal_type=MealType.Dinner
             ),
             PaprikaMealCalendar(
-                coordinator=entry.runtime_data.coordinator, meal_type=MealType.Lunch
+                coordinator=entry.runtime_data.coordinator, entry=entry, meal_type=MealType.Lunch
             ),
             PaprikaMealCalendar(
-                coordinator=entry.runtime_data.coordinator, meal_type=MealType.Breakfast
+                coordinator=entry.runtime_data.coordinator, entry=entry, meal_type=MealType.Breakfast
             ),
             PaprikaMealCalendar(
-                coordinator=entry.runtime_data.coordinator, meal_type=MealType.Snack
+                coordinator=entry.runtime_data.coordinator, entry=entry, meal_type=MealType.Snack
             ),
         ]
     )
